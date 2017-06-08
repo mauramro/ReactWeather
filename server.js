7e8b634d69e788e6a6557503530db8a97e5349bc
Mauricio Ramirez Rojas <mau.ram.ro@gmail.com>
2017-06-08 14:00:15 -0600

    Middleware update

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var express = require('express');
 // Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 app.use(function (req, res, next){
-  if (req.protocol === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + ':' + PORT + req.url);
+  } else {
+    next();
   }
 });
 
